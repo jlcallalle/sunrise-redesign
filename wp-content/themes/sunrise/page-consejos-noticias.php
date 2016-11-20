41e104d02533a986c6c13a87d4aaf35d1b3df3ee
Jorge Callalle <jlcallalle@gmail.com>
2016-11-20 15:20:51 -0500

    cambia position paginator

--- wp-content/themes/sunrise/page-consejos-noticias.php
+++ wp-content/themes/sunrise/page-consejos-noticias.php
@@ -22,7 +22,7 @@ get_header(); ?>
 	    )
 	  )
 	);
-	$allConsejos = new WP_Query( $args ); //obtengo las publicaciones del slug que indico 
+	$allConsejos = new WP_Query( $args ); //obtengo las publicaciones del slug que indico
 
     $post_per_page = 4;
     $paged = ( get_query_var( 'page' ) ) ? get_query_var( 'page' ) : 1;
@@ -36,55 +36,59 @@ get_header(); ?>
 <div class="listado-noticia" id="idConsejosNoticias" data-postperpage="<?php echo $post_per_page;?>">
 	<div class="container main clearfix">
 		<div class="iconos"></div>
-		<div class="clearfix  content">			
+		<div class="clearfix  content">
 				<div class="clearfix content_inner">
-					<div class="hfeed main_content">						 
-						<div class="clearfix main_content_inner">							
+					<div class="hfeed main_content">
+						<div class="clearfix main_content_inner">
 							<div class="content_main clearfix">
-								<div class="content_left_inner">									                    
+								<div class="content_left_inner">
 									<?php if($allConsejos->posts): ?>
-					                    <div class="main_consejos">
-					                      <h2 class="noticia_rugrats"><?php if($post->ID == 333) echo "Noticias"; else echo "Consejos"; ?></h2>
-					                      <div class="contenido_inner">
-					                      	<?php foreach($allConsejos->posts as $key => $consejo): ?>	
-					                      		<?php $rows++; if($rows < $min) { continue; }  if($rows > $max) { break; }  ?>  			                      	
-						                        <div class="box-noticia">
-						                          <div class="content_noticias"><a href="<?php echo get_permalink($consejo->ID); ?>" class="title_second"><?php echo get_the_title($consejo->ID); ?></a>
-						                            <p><a href="<?php echo get_field('imagen_consejo_noticia',$consejo->ID); ?>"><img src="<?php echo get_field('imagen_consejo_noticia',$consejo->ID); ?>" alt="collage-modificado" width="391" height="288" class="alignnone wp-image-286"></a></p>
-						                          </div>
-												  <div class="number"><em><strong><?php echo $consejo->post_content; ?></strong></em></div>						                          
-						                          <a href="<?php echo get_permalink($consejo->ID); ?>" class="link">Ver más</a>
-						                        </div>								
-											<?php endforeach; ?>
-					                      </div>
-					                    </div>
-					                    <?php ($post->ID == 331) ? $id=333 : $id=331; ?>
-					                    <a class="verpost" href="<?php echo get_permalink($id); ?>" >Ver <?php if($post->ID == 331) echo "Noticias"; else echo "Consejo"; ?></a>
-				                    <?php endif; ?>				                    
+			                    <div class="main_consejos">
+			                      <h2 class="noticia_rugrats"><?php if($post->ID == 333) echo "Noticias"; else echo "Consejos"; ?></h2>
+			                      <div class="contenido_inner clearfix">
+			                      	<?php foreach($allConsejos->posts as $key => $consejo): ?>
+			                      		<?php $rows++; if($rows < $min) { continue; }  if($rows > $max) { break; }  ?>
+				                        <div class="box-noticia">
+				                          <div class="content_noticias"><a href="<?php echo get_permalink($consejo->ID); ?>" class="title_second"><?php echo get_the_title($consejo->ID); ?></a>
+				                            <p><a href="<?php echo get_field('imagen_consejo_noticia',$consejo->ID); ?>"><img src="<?php echo get_field('imagen_consejo_noticia',$consejo->ID); ?>" alt="collage-modificado" width="391" height="288" class="alignnone wp-image-286"></a></p>
+				                          </div>
+										  						<div class="number"><em><strong><?php echo $consejo->post_content; ?></strong></em></div>
+				                          <a href="<?php echo get_permalink($consejo->ID); ?>" class="link">Ver más</a>
+				                        </div>
+															<?php endforeach; ?>
+			                      </div>
+														<?php
+															$link_page = paginate_links( array(
+																'format' => '?paged=%#%',
+																'current' => $paged,
+																'total' => $pages,
+																'type' => 'plain'
+															) );
+
+														//echo "<pre>";
+														//print_r($link_page);
+														//echo "</pre>";
+														?>
+														<div class="pagination">
+															<?php echo $link_page; ?>
+														</div>
+
+
+			                    </div>
+			                    <?php ($post->ID == 331) ? $id=333 : $id=331; ?>
+			                    <a class="verpost" href="<?php echo get_permalink($id); ?>" >Ver <?php if($post->ID == 331) echo "Noticias"; else echo "Consejo"; ?></a>
+		                    <?php endif; ?>
 								</div>
 							</div>
-							<?php //get_sidebar(); ?>							
-						</div> 				
+							<?php //get_sidebar(); ?>
+						</div>
 					</div>
-				</div><!--End content_inner -->			
-		</div><!--End content -->				
+				</div><!--End content_inner -->
+		</div><!--End content -->
 
-        <?php       
-          $link_page = paginate_links( array(   
-            'format' => '?paged=%#%',
-            'current' => $paged,
-            'total' => $pages,
-            'type' => 'plain'
-          ) );
 
-        //echo "<pre>";
-        //print_r($link_page);
-        //echo "</pre>";
-        ?>
 	</div>
-	<div class="pagination">
-	  <?php echo $link_page; ?>
-	</div>
+
 </div>
 <input type="hidden" name="idlista" id="idlista" value="<?php echo $post->ID; ?>">
 <?php get_footer(); ?>
