47eec61cd4cd7fd116ce2aa6b36bb20a4308a662
Jorge Callalle <jlcallalle@medialabla.com>
2017-01-13 10:48:00 -0500

    se agrega vieport en header.php

--- wp-content/themes/sunrise/header.php
+++ wp-content/themes/sunrise/header.php
@@ -35,7 +35,7 @@
 
 <!-- Mobile viewport optimized: h5bp.com/viewport -->
 <meta name="keywords" content="<?php bloginfo('description'); ?>" />
-<!-- <meta name="viewport" content="width=device-width, initial-scale=1.0"> -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
 
 <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
