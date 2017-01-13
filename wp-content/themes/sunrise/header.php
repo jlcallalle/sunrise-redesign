e6b01df2cbb67c8b33eff0645eea467e9048727d
Jorge Callalle <jlcallalle@medialabla.com>
2017-01-13 11:19:23 -0500

    restriccion de pagina form slider

--- wp-content/themes/sunrise/header.php
+++ wp-content/themes/sunrise/header.php
@@ -294,4 +294,11 @@ if (typeof jQuery == 'undefined')
 
 
 		</div>
-		<div class="content_slider_mobile"><img src="<?php bloginfo('template_directory');?>/images/slider-mobile.png" alt=""></div>
+		<?php
+			if ( !is_page('26') or (!is_page('29')) ) {	
+				echo '<div class="content_slider_mobile">
+					<img src='.bloginfo('template_directory').'/images/slider-mobile.png" alt="">  
+				</div>'		
+			} 
+		?>
+
